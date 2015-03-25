998a1ba62fa46736dde52e95ef90a8d855497b08
Oskrin <oskrs1234>
2015-03-25 15:34:21 -0500

    menu

--- menus/menu.php
+++ menus/menu.php
@@ -92,7 +92,7 @@ function menu_1() {
             <li><a href="" target="_parent"><i class="icon-book"></i> Mantenimiento</a>
                 <ul>
                     <li><a href="../plan_cuentas" target="_blank">Registro Equipos</a></li>
-                    <li><a href="../transacciones" target="_blank">Reparación Equipos/a></li>
+                    <li><a href="../transacciones" target="_blank">Reparación Equipos</a></li>
                     <li><a href="../kardex" target="_blank">Entrega Equipos</a></li>
                     <li><a href="../libro_diario" target="_blank">Reestablecer</a></li>
                 </ul>
