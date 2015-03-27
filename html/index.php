b65088437d364e34404f8d331d2428113e13e40d
deividscriollo <deividscriollo@gmail.com>
2015-03-27 17:28:25 -0500

    return

--- html/index.php
+++ html/index.php
@@ -13,8 +13,6 @@ session_destroy();
         <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="../css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" type="text/css" href="../css/jquery-ui-1.10.4.custom.css"/> 
-
-
         <link href="../css/font-awesome.css" rel="stylesheet">
 
         <link href="../css/style.css" rel="stylesheet" type="text/css">
@@ -22,12 +20,8 @@ session_destroy();
         <link rel="stylesheet" href="../css/alertify.core.css" />
         <link rel="stylesheet" href="../css/alertify.default.css" id="toggleCSS" />
         <link rel="stylesheet" href="../css/logo.css" id="dclogo" />
-        <link rel="stylesheet" type="text/css" href="../css/dccolor.css"/> 
 
         <!--<script src="js/jquery-1.7.2.min.js"></script>-->
-        <link rel="dns-prefetch" href="//fonts.googleapis.com" />
-  <!-- <link rel="dns-prefetch" href="//code.jquery.com" /> -->
-
         <script src="../js/bootstrap.js"></script>
         <script type="text/javascript" src="../js/jquery-1.10.2.js"></script>
         <script type="text/javascript" src="../js/jquery-ui-1.10.4.custom.min.js"></script>
@@ -41,44 +35,35 @@ session_destroy();
           -webkit-background-size: cover;
           -moz-background-size: cover;
           -o-background-size: cover;
-          background-size: cover;"> 
-          <header class="site__header island">
-                                  <div class="wrap">
-                                   <span id="animationSandbox" style="display: block;"><h1 class="site__title mega">Acceder</h1></span>
-                                  </div>
-                                </header><!-- /.site__header -->
+          background-size: cover;">
+        <div class="account-container">
+            <div class="content clearfix">
+                <form action="" method="post" name="form_admin">
+                    <h1>Usuario</h1>
+                    <div class="login-fields">
+                        <p>Por favor, proporcione sus datos</p>
+                        <div class="field">
+                            <label for="username">Usuario:</label>
+                            <input type="text" id="txt_usuario" name="txt_usuario" placeholder="Usuario" class="login username-field" />
+                        </div> <!-- /field -->
 
-        <div class="row-fluid">
-            <div class="span4">
-                <img src="../images/usuario.fw.png" width="100%">
+                        <div class="field">
+                            <label for="password">Password:</label>
+                            <input type="password" id="txt_contra" name="txt_contra" placeholder="Constraseña" class="login password-field"/>
+                        </div>
+                    </div>
+
+                    <div class="login-actions">
+                        <span class="login-checkbox">
+                            <input id="Field" name="Field" type="checkbox" class="field login-checkbox" value="First Choice" tabindex="4" />
+                            <label class="choice" for="Field">Recordar conexión</label>
+                        </span>
+
+                        <button class="button btn btn-success btn-large" id="btnIngreso" >Ingresar</button>
+                        <button class="button btn btn-success btn-large" id="btnRetornar">Retornar</button>
+                    </div>
+                </form>
             </div>
-            <div class="span8">
-            
-                        <form action="" method="post" name="form_admin">                           
-                            <div class="row-fluid">
-                                 <div class="login-fields">                                    
-                                    <div class="field">
-                                        <label for="username">Usuario:</label>
-                                        <input type="text" id="txt_usuario" name="txt_usuario" placeholder="Usuario" class="login username-field" />
-                                    </div> <!-- /field -->
-
-                                    <div class="field">
-                                        <label for="password">Password:</label>
-                                        <input type="password" id="txt_contra" name="txt_contra" placeholder="Constraseña" class="login password-field"/>
-                                    </div>
-                                </div>
-                            </div>                          
-
-                            <div class="login-actions pull-left">    
-                                <button class="butt js--triggerAnimation" id="btnRetornar">Retornar</button>                                                      
-                                <button class="butt js--triggerAnimation" id="btnIngreso">Ingresar</button>                                
-                            </div>
-                        </form>
-                       
-                        
-            </div>
-        </div>       
-
-      
+        </div> 
     </body>
 </html>
